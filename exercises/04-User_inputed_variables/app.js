2386147bec7eb3f520d645faab5bf2ec96af9dcb
Karoll Guzmán <karolldguzman@gmail.com>
2024-10-24 00:09:48 +0000

    Add console.log statement to print 'Hello World' in 01-Hello_World/app.js Add console.log statement to print 'red' in 02-Print_variables_in_the_console/app.js Add multiplication code in 03-Multiply_two_values/app.js Add console.log statement to calculate age in ten years in 04-User_inputed_variables/app.js Change VERSION variable from const to let in 05-Constants/app.js Change myVar1 and myVar2 values to 'Hello' and 'World' respectively in 06-String_concatenation/app.js Change htmlDocument value in 07-Create_a_basic_HTML/app.js Add console.log statement to call isOdd function with argument 45345 in 08-Calling_your_first_function/app.js Change addNumbers function to return the sum of two numbers in 09-Creating_your_first_function/app.js Change shortIntroduction function to accept three arguments and return a string in 10-Create_a_new_function/app.js Add if statements to print different messages based on the value of total in 11-Your_first_if/app.js Change getPrice function to calculate the cost of a wedding based on the number of guests in 12-How_much_the_wedding_costs/app.js Add getColor function and prompt user for a color in 13-Your_first_switch/app.js

--- exercises/04-User_inputed_variables/app.js
+++ exercises/04-User_inputed_variables/app.js
@@ -1,3 +1,5 @@
 let age = prompt('What is your age?');
 
 // Your code below:
+let ageInTenYears = Number(age) + 10;
+console.log(ageInTenYears);
--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,7 +1,7 @@
-function shortIntroduction() {
-  // Complete this function's body and arguments
+function shortIntroduction(myName, myProfession, myAge) { 
+ return "Hello! my name is " + myName + ", my profession is " + myProfession + ". I am " + myAge + " years old."
   
 }
 
 // Fill the gaps with your data in the correct order
-console.log(shortIntroduction(" ", " ", " "))
+console.log(shortIntroduction("Karoll ", " developer", " 27"));
